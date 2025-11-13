a8b8955a3f91efc12aac63f53d5b89091e8862c4
Rohit mhetre <rohitmhetre2004@gmail.com>
2025-11-14 02:36:15 +0530

    Update api.js

--- alumni-panel/src/config/api.js
+++ alumni-panel/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://campusconnectbackend-in.onrender.com';
 
 // Create axios instance
 const api = axios.create({
