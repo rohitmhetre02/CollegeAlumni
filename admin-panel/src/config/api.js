dfbd94920f95d5893f3afd9e5b031fc7dd3109d9
Rohit mhetre <rohitmhetre2004@gmail.com>
2025-11-14 02:38:31 +0530

    Update api.js

--- admin-panel/src/config/api.js
+++ admin-panel/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://campusconnectbackend-in.onrender.com';
+const API_URL = 'https://campusconnectbackend-in.onrender.com/';
 
 const api = axios.create({
   baseURL: API_URL,
